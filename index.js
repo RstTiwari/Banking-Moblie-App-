6442edb0b6985caada2c3541aba7facf7b8b4004
Rohit Tiwari <iamrrt2121@gmail.com>
2022-01-19 12:57:49 +0530

    new changes

--- index.js
+++ index.js
@@ -37,6 +37,7 @@ function fixedbar(){
         navbar.classList.add("sticky__header")
         opnBtn.classList.remove("active")
         header.classList.remove("active")
+        overlap.classList.remove("active")
     }else{
         navbar.classList.remove("sticky__header")
     }
@@ -47,25 +48,33 @@ var opnBtn = document.getElementById("open__btn")
 var header= document.getElementById("sidenav__bar")
 var clsBtn = document.getElementById("close__btn")
 var navlink = document.querySelectorAll(".sidenav__link")
+var overlap =document.getElementById("overlap")
+
+
+
 
 navlink.forEach(n => n.addEventListener('click', closemenu))
-
-
 opnBtn.addEventListener('click' , openmenu)
-
+clsBtn.addEventListener('click' , closemenu)
+overlap.addEventListener('click' , closemenu)
 
 function openmenu() {
     opnBtn.classList.toggle("active")
     header.classList.toggle("active")
+    overlap.classList.toggle("active")
+     
+    
 }
 
-clsBtn.addEventListener('click' , closemenu)
 
 function closemenu() {
     opnBtn.classList.remove("active")
     header.classList.remove("active")
+    overlap.classList.remove("active")
+   
 }
-body.onclick =function() {closemenu()};
+   
+ 
 
 
 
@@ -87,31 +96,9 @@ body.onclick =function() {closemenu()};
 
 
 
-// var slideIndex = 1;
 
 
-// function showSlides(n) {
-//   var i;
-//   var slides = document.getElementsByClassName("mySlides");
-//   var dots = document.getElementsByClassName("dot");
-//   if (n > slides.length) {slideIndex = 1}    
-//   if (n < 1) {slideIndex = slides.length}
-//   for (i = 0; i < slides.length; i++) {
-//       slides[i].style.display = "none";  
-//   }
-//   for (i = 0; i < dots.length; i++) {
-//       dots[i].className = dots[i].className.replace(" unactive", "");
-//   }
-//   slides[slideIndex-1].style.display = "block";  
-//   dots[slideIndex-1].className += " unactive";
-// }
 
-// showSlides(slideIndex);
 
-// function plusSlides(n) {
-//   showSlides(slideIndex += n);
-// }
 
-// function currentSlide(n) {
-//   showSlides(slideIndex = n);
-// }
+
